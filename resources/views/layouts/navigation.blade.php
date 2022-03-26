b50c2503f973cb25d366eff3b8c0ce34c532dbe0
motoki827 <motoki827@gmail.com>
2022-03-26 20:07:41 +0900

    ロゴ変更

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <img src="{{asset('logo/logo.jpg')}}" class="mt-4" style="max-height:80px">
+                        <!-- <x-application-logo class="block h-10 w-auto fill-current text-gray-600" /> -->
                     </a>
                 </div>
 
