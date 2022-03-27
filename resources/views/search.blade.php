2b2aad553bf074de380cc41f8817619c7716f77a
motoki827 <motoki827@gmail.com>
2022-03-27 21:39:53 +0900

    検索ページ　レイアウト調整

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -14,7 +14,7 @@
                         <div class="container mx-auto bg-gray-500 rounded-lg p-14">
                             <form action="/search_result" method="post">
                                 @csrf
-                                <h1 class="text-center font-bold text-white text-4xl">練習メニュー検索</label>
+                                <h1 class="text-center font-bold text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl">練習メニュー検索</label>
                                     <p class="mx-auto font-normal text-sm my-6 max-w-lg">メニュー名検索</p>
                                     <div class="sm:flex items-center bg-white rounded-lg overflow-hidden px-2 py-1 justify-between">
                                         <input name="menu_name" class="text-base text-gray-400 flex-grow outline-none px-2 " type="text" placeholder="練習メニュー" />
