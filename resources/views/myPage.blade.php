80387a8bbc7c51968792607ba14b7ec52e4a147a
motoki827 <motoki827@gmail.com>
2022-04-01 09:00:49 +0900

    マイページスタイル調整

--- resources/views/myPage.blade.php
+++ resources/views/myPage.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-           
+
         </h2>
         <x-message :message="session('message')" />
     </x-slot>
@@ -10,41 +10,20 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <style>
-                        :root {
-                            --main-color: #4a76a8;
-                        }
-
-                        .bg-main-color {
-                            background-color: var(--main-color);
-                        }
-
-                        .text-main-color {
-                            color: var(--main-color);
-                        }
-
-                        .border-main-color {
-                            border-color: var(--main-color);
-                        }
-                    </style>
-                    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-                    <!-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script> -->
-
-
 
                     <div class="bg-gray-100">
-                        <div class="w-full text-white bg-main-color">
-                            <div x-data="{ open: false }" class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
-                                <div class="p-4 flex flex-row items-center justify-between">
-                                    <a href="#" class="text-lg font-semibold tracking-widest uppercase rounded-lg focus:outline-none focus:shadow-outline">マイページ</a>
+                        <div class="w-full text-white bg-blue-500">
+                            <div x-data="{ open: false }" class="flex max-w-screen-xl px-4 mx-auto md:items-center justify-between flex-row md:px-6 lg:px-8">
+                                <div class="p-4 flex items-center justify-between">
+                                    <a href="#" class="md:text-lg text-sm font-semibold tracking-widest uppercase rounded-lg focus:outline-none focus:shadow-outline">マイページ</a>
                                     <!-- <button class="md:hidden rounded-lg focus:outline-none focus:shadow-outline" @click="open = !open">
-                                        <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
-                                            <path x-show="!open" fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path>
-                                            <path x-show="open" fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                                        </svg>
+                    <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
+                        <path x-show="!open" fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+                        <path x-show="open" fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                    </svg>
                                     </button> -->
                                 </div>
-                                <nav :class="{'flex': open, 'hidden': !open}" class="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row">
+                                <nav  class="flex-grow pb-4 md:pb-0 flex justify-end">
                                     <div @click.away="open = false" class="relative" x-data="{ open: false }">
                                         <button @click="open = !open" class="flex flex-row items-center space-x-2 w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent hover:bg-blue-800 md:w-auto md:inline md:mt-0 md:ml-4 hover:bg-gray-200 focus:bg-blue-800 focus:outline-none focus:shadow-outline">
                                             <span>{{$user['name']}}</span>
@@ -54,7 +33,7 @@
                                             </svg>
                                         </button>
                                         <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
-                                            <div class="py-2 bg-white text-blue-800 text-sm rounded-sm border border-main-color shadow-sm">
+                                            <div class="py-2 bg-white text-blue-800 text-sm rounded-sm border shadow-sm">
                                                 <a class="block px-4 py-2 mt-2 text-sm bg-white md:mt-0 focus:text-gray-900 hover:bg-indigo-100 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="/profile_card_edit">プロフィールの編集</a>
                                                 <a class="block px-4 py-2 mt-2 text-sm bg-white md:mt-0 focus:text-gray-900 hover:bg-indigo-100 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Help</a>
                                                 <div class="border-b"></div>
@@ -70,27 +49,27 @@
                         <div class="container mx-auto my-5 p-5">
                             <div class="md:flex no-wrap md:-mx-2 ">
                                 <!-- Left Side -->
-                                <div class=" md:w-3/12 md:mx-2">
+                                <div class=" md:w-3/12 md:mx-2 lg:w-2/12">
                                     <!-- Profile Card -->
                                     <div class="flex md:block bg-white p-3 border-t-4 border-green-400">
                                         <div class="image overflow-hidden w-1/4 md:w-full">
-                                            <img class="h-auto  md:w-full mx-auto " src="{{'/storage/' . $user['image_name']}}" alt="">
+                                            <img class="mx-auto " src="{{'/storage/' . $user['image_name']}}" alt="">
                                             <!-- <img class="h-auto  md:w-full mx-auto " src="{`https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png`}" alt=""> -->
                                         </div>
                                         <div class="mx-4">
-                                            <h1 class="text-gray-900 font-bold text-xl leading-8 my-1">{{$user['name']}}</h1>
+                                            <h1 class="text-gray-900 font-bold text-lg leading-8 my-1">{{$user['name']}}</h1>
                                             <h3 class="text-gray-600 font-lg text-semibold leading-6">コメント</h3>
                                             <p class="text-sm text-gray-500 hover:text-gray-600 leading-6">コメント</p>
                                         </div>
                                     </div>
-                            
+
                                     <!-- End of profile card -->
                                 </div>
                                 <!-- Right Side -->
                                 <div class="w-full md:w-9/12 mx-2 h-100">
                                     <!-- Profile tab -->
                                     <!-- About Section -->
-                                    <div class="bg-white p-3 shadow-sm rounded-sm mx-auto">
+                                    <!-- <div class="bg-white p-3 shadow-sm rounded-sm mx-auto">
                                         <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
                                             <span clas="text-green-500">
                                                 <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -114,14 +93,13 @@
                                             </div>
                                         </div>
                                         <button class="block w-full text-blue-800 text-sm font-semibold rounded-lg hover:bg-gray-100 focus:outline-none focus:shadow-outline focus:bg-gray-100 hover:shadow-xs p-3 my-4">もっと見る</button>
-                                    </div>
+                                    </div> -->
                                     <!-- End of about section -->
 
-                                    <div class="my-4"></div>
+                                    <!-- <div class="my-4"></div> -->
 
                                     <!-- Experience and education -->
-                                    <div class="bg-white p-3 shadow-sm rounded-sm">
-
+                                    <div class="bg-white p-3 shadow-sm rounded-sm mt-3 md:m-0 text-sm md:text-lg ">
                                         <div class="grid grid-cols-2">
                                             <div>
                                                 <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8 mb-3">
@@ -153,9 +131,9 @@
                                                     </span>
                                                     <span class="tracking-wide">お気に入り</span>
                                                 </div>
-                                                <form action="/create_today_menu" method="post" >
-                                                @csrf    
-                                                     <ul class="list-inside space-y-2">
+                                                <form action="/create_today_menu" method="post">
+                                                    @csrf
+                                                    <ul class="list-inside space-y-2">
                                                         @foreach($favorite_menus as $favorite_menu)
                                                         <li>
                                                             <div class="text-teal-600">
@@ -181,4 +159,6 @@
             </div>
         </div>
     </div>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <!-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script> -->
 </x-app-layout>
