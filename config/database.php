d792c68e7c29a9d5a233ac22f6070c31e1fc1cec
motoki827 <motoki827@gmail.com>
2022-04-23 16:52:00 +0900

    heroku設定

--- config/database.php
+++ config/database.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Str;
 
-$db = parse_url(env('DATABASE_URL'));
 return [
-
 
     /*
     |--------------------------------------------------------------------------
@@ -34,6 +32,7 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
+
     'connections' => [
 
         'sqlite' => [
@@ -47,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => $db['host'],
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' =>ltrim($db["pass"],'/'),
-            'username' => $db['user'],
-            'password' => $db['pass'],
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -130,6 +129,7 @@ return [
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
@@ -138,6 +138,7 @@ return [
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
