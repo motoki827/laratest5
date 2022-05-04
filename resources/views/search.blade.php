664d577fe28c6dbc296fa451e8d4235ec0e44457
motoki827 <motoki827@gmail.com>
2022-05-04 10:49:19 +0900

    スタイル調整

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -5,44 +5,37 @@
     </x-slot>
 
     <div class="py-6">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <div class=" bg-gray-100 flex justify-center">
-                        <div class="container mx-auto bg-gray-500 rounded-lg p-14">
-                            <form action="/search_result" method="post">
-                                @csrf
-                                <h1 class="text-center font-bold text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl">練習メニュー検索</label>
-                                    <p class="mx-auto font-normal text-sm md:text-2xl my-6 max-w-lg">メニュー名検索</p>
-                                    <div class="sm:flex items-center bg-white rounded-lg overflow-hidden px-2 py-1 justify-between">
-                                        <input name="menu_name" class="text-base text-gray-400 flex-grow outline-none px-2 " type="text" placeholder="練習メニュー" />
-                                        <div class="ms:flex items-center px-2 rounded-lg space-x-4 mx-auto ">
-                                            <button class="bg-indigo-500 text-white text-base rounded-lg px-4 py-2 font-thin">検索</button>
-                                        </div>
-                                    </div>
-                            </form>
-                            <form action="/search_result" method="post">
-                                @csrf
-                                <p class="mx-auto font-normal text-sm my-6 md:text-2xl max-w-lg">ジャンル検索</p>
-                                <div class="sm:flex items-center  rounded-lg overflow-hidden px-2 py-1 justify-between">
-                                    <div class="bg-white ms:flex items-center px-2 py-1 rounded-lg space-x-4 mx-auto ">
-                                        <select name="genre" class="text-base text-gray-800 outline-none border-2 py-2 rounded-lg">
-                                            <option></option>
-                                            <option value="パス">パス</option>
-                                            <option value="スパイク">スパイク</option>
-                                            <option value="レシーブ">レシーブ</option>
-                                            <option value="ゲーム">ゲーム</option>
-                                            <option value="トレーニング">トレーニング</option>
-                                            <option value="サーブ">サーブ</option>
-                                        </select>
-                                        <button class="bg-indigo-500 text-white text-base rounded-lg px-4 py-2 font-thin">検索</button>
-                                    </div>
-                                </div>
-                            </form>
+        <div class="container mx-auto bg-gray-400 rounded-lg p-4">
+            <form action="/search_result" method="post">
+                @csrf
+                <h1 class="text-center font-bold text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl">練習メニュー検索</label>
+                    <p class="mx-auto font-normal text-ms md:text-2xl mt-6 max-w-lg">メニュー名検索</p>
+                    <div class="ms:flex items-center">
+                        <div class="px-2 rounded-lg mx-auto flex justify-center">
+                            <input name="menu_name" class="mr-1 text-base text-gray-400 flex-grow max-w-xl outline-none px-2 mb-2 rounded-lg" type="text" placeholder="練習メニュー" />
+                            <button class="bg-indigo-500 text-white text-base rounded-lg px-4 font-thin">検索</button>
                         </div>
                     </div>
+            </form>
+            <form action="/search_result" method="post">
+                @csrf
+                <p class="mx-auto font-normal text-ms mt-6 md:text-2xl max-w-lg">ジャンル検索</p>
+                <div class="rounded-lg px-2 py-1">
+                    <div class="ms:flex items-center">
+                        <select name="genre" class="text-base text-gray-800 outline-none border-2 py-2 rounded-lg mb-2">
+                            <option></option>
+                            <option value="パス">パス</option>
+                            <option value="スパイク">スパイク</option>
+                            <option value="レシーブ">レシーブ</option>
+                            <option value="ゲーム">ゲーム</option>
+                            <option value="トレーニング">トレーニング</option>
+                            <option value="サーブ">サーブ</option>
+                        </select>
+                        <button class="bg-indigo-500 text-white text-base rounded-lg px-4 py-2 font-thin">検索</button>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
+
     </div>
 </x-app-layout>
